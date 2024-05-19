ac0aaf95b1dd3516f24052b084d19ea77d719532
Dechanel DIPEUA <dechaneldipeua@gmail.com>
2024-05-19 12:53:52 +0100

    init

--- src/main/java/com/openclassroom/repository/DBUserRepository.java
+++ src/main/java/com/openclassroom/repository/DBUserRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface DBUserRepository extends JpaRepository<DBUser, Integer> {
     public DBUser findByUsername(String username);
+    //
 }
